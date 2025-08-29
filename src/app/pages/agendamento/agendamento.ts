96cd05b40c33baf1445332985dc821d2da02a69f
Lívia Nöer <lilidulliusnoer@gmail.com>
2025-08-29 13:44:44 -0300

    arrumando botões

--- src/app/pages/agendamento/agendamento.ts
+++ src/app/pages/agendamento/agendamento.ts
@@ -6,7 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
