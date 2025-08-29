781dc47c9ee67ed0c3abe1e2ef558dcc2ff1d2d2
JuTietbohl <juliatietbohl05@gmail.com>
2025-08-29 15:34:12 -0300

    Feature/cores (#9)
    * Request graphs resources
    
    ---------
    
    Co-authored-by: Júlia Teixeira Tietbohl <julia.tietbohl002@edu.pucrs.br>

--- src/app/pages/estatisticas/estatisticas.ts
+++ src/app/pages/estatisticas/estatisticas.ts
@@ -5,10 +5,23 @@ import { LaboratorioService } from '../../services/laboratorio.service';
 import { NotebookService } from '../../services/notebook.service';
 import { SalaService } from '../../services/sala.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Notebook } from '../../models/notebook';
+import { Laboratorio } from '../../models/laboratorio';
+import { Sala } from '../../models/sala';
+import { RecursofuncionarioService } from '../../services/recursofuncionario.service';
+import { RecursoFuncionario } from '../../models/recursoFuncionario';
 
 interface Data {
   name: String,
   value: number
+}
+
+enum DiaSemana {
+  Segunda = 1,
+  Terca = 2,
+  Quarta = 3,
+  Quinta = 4,
+  Sexta = 5,
 }
 
 @Component({
@@ -27,50 +40,72 @@ export class EstatisticasComponent {
   showLegend = false;
   showXAxisLabel = false;
   showYAxisLabel = false;
+  moreFrequent = '#E9C46A';
+  lessFrequent = '#2A9D8F';
+
+  notebooks: RecursoFuncionario[] = [];
+  laboratorios: RecursoFuncionario[] = [];
+  salas: RecursoFuncionario[] = [];
+  resources = new Set<Data>();
+  dias = new Set<Data>();
 
   constructor(
-    private labService: LaboratorioService,
-    private notebookService: NotebookService,
-    private salaService: SalaService
-  ){}
+    private recursoFuncionarioService: RecursofuncionarioService
+  ) { }
 
-  dias: Data[] = [
-    {
-      "name": "Segunda-Feira",
-      "value": 13
-    },
-    {
-      "name": "Terça-Feira",
-      "value": 2
-    },
-    {
-      "name": "Quarta-Feira",
-      "value": 5
-    },
-    {
-      "name": "Quinta-Feira",
-      "value": 17
-    },
-    {
-      "name": "Sexta-Feira",
-      "value": 8
-    }
-  ];
+  // 0 = sala /  1 = lab / 2 = note
 
-  resources: Data[] = [
-    {
-      "name": "Notebook",
-      "value": 25
-    },
-    {
-      "name": "Laboratório",
-      "value": 13
-    },
-    {
-      "name": "Sala",
-      "value": 7
-    }
-  ];
+  ngOnInit(): void {
+
+    // req
+
+    this.recursoFuncionarioService.getRecursosFuncionarios().subscribe((data) => {
+      data.forEach(d => {
+        switch (d.TipoRecurso) {
+          case 0:
+            this.salas.push(d);
+            break;
+          case 1:
+            this.laboratorios.push(d);
+            break;
+          case 2:
+            this.notebooks.push(d);
+            break;
+        }
+      });
+    })
+
+    this.salas.forEach(_ => {
+      this.resources.has({ name: 'Sala', value: 0 }) ? this.resources.forEach(resource => {
+        if (resource.name === 'Sala') {
+          resource.value += 1;
+        }
+      }) : this.resources.add({ name: 'Sala', value: 1 })
+    });
+
+    this.laboratorios.forEach(_ => {
+      this.resources.has({ name: 'Laboratorio', value: 0 }) ? this.resources.forEach(resource => {
+        if (resource.name === 'Laboratorio') {
+          resource.value += 1;
+        }
+      }) : this.resources.add({ name: 'Laboratorio', value: 1 })
+    });
+
+    this.notebooks.forEach(_ => {
+      this.resources.has({ name: 'Notebook', value: 0 }) ? this.resources.forEach(resource => {
+        if (resource.name === 'Notebook') {
+          resource.value += 1;
+        }
+      }) : this.resources.add({ name: 'Notebook', value: 1 })
+    });
+
+
+    
+
+
+  }
+
+
 
 
 }
